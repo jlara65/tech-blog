05194b9d95a746ca9301857a626b42b57163d5e3
Joseph Lara <vipercar233@gmail.comgit config --global user.name Joseph>
2022-10-13 01:48:52 -0500

    Added db user.init table and added user-routes in api

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const exp = require('constants');
 const helpers = require('./utils/helpers');
 
 const app = express();
