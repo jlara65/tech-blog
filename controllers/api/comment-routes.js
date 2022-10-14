ad78bab027abf6ff87975afa6417ad22da9881c1
Joseph Lara <vipercar233@gmail.comgit config --global user.name Joseph>
2022-10-13 20:58:02 -0500

    Added comments models, api and routes

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+
+// GET
+router.get('/', (req, res) => {});
+
+// POST
+router.post('/', (req, res) => {});
+
+// delete
+router.get('/:id', (req, res) => {});
+
+module.exports = router;
