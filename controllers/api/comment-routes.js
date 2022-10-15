50f7ada246cad93e78d3f9c72b03f9a2bff9c7e3
Joseph Lara <vipercar233@gmail.comgit config --global user.name Joseph>
2022-10-14 22:24:27 -0500

    Added comments routes and partials

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,13 +1,48 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 
-// GET
-router.get('/', (req, res) => {});
+// GET /api/comments
+router.get('/', (req, res) => {
+  Comment.findAll({})
+    .then((dbCommentData) => res.json(dbCommentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 // POST
-router.post('/', (req, res) => {});
+router.post('/', (req, res) => {
+  Comment.create({
+    comment_text: req.body.comment_text,
+    post_id: req.body.post_id,
+    user_id: req.body.user_id,
+  })
+    .then((dbCommentData) => res.json(dbCommentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
 
 // delete
-router.get('/:id', (req, res) => {});
+router.get('/:id', (req, res) => {
+  Comment.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbCommentData) => {
+      if (!dbCommentData) {
+        res.status(404).json({ message: 'No comment found with this id' });
+        return;
+      }
+      res.json(dbCommentData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
